da44b148a12894f63a856f2be5cc9810ad3a8b42
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-06-24 16:38:14 +0100

    Modify like.rb to add validation

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,11 +2,16 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
-  after_create :update_post_likes_counter
+  after_save :increment_likes_counter
+  after_destroy :decrement_likes_counter
 
   private
 
-  def update_post_likes_counter
-    post.update(likes_counter: post.likes.count)
+  def increment_likes_counter
+    post.increment!(:likescounter)
+  end
+
+  def decrement_likes_counter
+    post.decrement!(:likescounter)
   end
 end
