21466224fa5ca7afa8088398291e2fa6f796908f
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-07-01 11:53:50 +0100

    Implement View requirements

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_22_090856) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_22_211727) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,7 +41,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_22_090856) do
     t.integer "likescounter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
     t.index ["author_id"], name: "index_posts_on_author_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
