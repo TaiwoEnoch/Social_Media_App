44173b160a287faa09a66faaef42fb96e6942f9b
David Igbo <95672514+david30-maker@users.noreply.github.com>
2023-07-07 22:11:54 +0100

    implement resources for routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ devise_for :users, controllers: { registrations: 'registrations' }
 
 root 'users#index'
 resources :users, only: [:index, :show] do
-    # resources :posts, only: [:index, :show, :new, :create] do
-    #   resources :comments, only: [:new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
       resources :likes, only: [:create]
     end
   end
